6b42308bc5577cf6471047c8cfe2c4fbe3c54465
JKukielski <jankukielski1@gmail.com>
2024-03-04 20:14:04 +0100

    Initial logic for enabling registration

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,22 +1,86 @@
+'use client';
+
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const Register = () => {
+  const [error, setError] = useState('');
+  const router = useRouter();
+  const isValidEmail = (email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    if (!isValidEmail(email)) {
+      setError('Invalid email');
+      return;
+    }
+
+    if (!password || password.length < 8) {
+      setError('Invalid password');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (response === 400) {
+        setError('Email  already in use.');
+      }
+      if (response.status === 200) {
+        setError('');
+        router.push('/login');
+      }
+    } catch (error) {
+      setError('Try again.');
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <div>
         <h1>Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="name">
             Name
-            <input type="text" id="name" />
+            <input type="text" id="name" name="name" className="text-black" />
           </label>
           <label htmlFor="email">
             Email
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="text-black"
+            />
           </label>
           <label htmlFor="password">
             Password
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="text-black"
+            />
           </label>
           <button type="submit">Register</button>
         </form>
