f5c28cc6242be9e7dc40c562563610247df5b780
JKukielski <jankukielski1@gmail.com>
2024-03-20 18:01:20 +0100

    Initial navbar structure

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,4 @@
-import {
-  BsSearch,
-  BsCart3,
-  BsPerson,
-  BsHeart,
-  BsBox2HeartFill,
-} from 'react-icons/bs';
+import { BiHeart, BiUser, BiCart, BiSearch, BiPackage } from 'react-icons/bi';
 import './navbar.css';
 
 const Navbar = () => {
@@ -12,16 +6,30 @@ const Navbar = () => {
     <div className="navbar-container">
       <div className="navbar-container-top">
         <div className="navbar-logo-container">
-          <BsBox2HeartFill className="navbar-logo-icon" />
+          <BiPackage className="navbar-logo-icon" />
           <h1 className="navbar-logo-text">SHOPPER</h1>
         </div>
         <div className="navbar-icon-container">
-          <BsPerson className="navbar-icon" />
-          <BsHeart className="navbar-icon" />
-          <BsCart3 className="navbar-icon" />
+          <BiUser className="navbar-icon" />
+          <BiHeart className="navbar-icon" />
+          <BiCart className="navbar-icon" />
         </div>
       </div>
-      <div className="navbar-container-bottom"></div>
+      <div className="navbar-container-bottom">
+        <ul className="navbar-categories">
+          <li className="navbar-category">Man</li>
+          <li className="navbar-category">Woman</li>
+          <li className="navbar-category">Accessories</li>
+        </ul>
+        <div className="navbar-search-container">
+          <input
+            className="navbar-search-input"
+            type="text"
+            placeholder="Search..."
+          />
+          <BiSearch className="navbar-search-icon" />
+        </div>
+      </div>
     </div>
   );
 };
