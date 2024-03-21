dc83e2647dce3ae4d55e6fea297fba6ba3fac3c9
JKukielski <jankukielski1@gmail.com>
2024-03-21 19:28:32 +0100

    Placeholder data for hero section slider

---
+++ utils/data.js
@@ -0,0 +1,17 @@
+export const heroData = [
+  {
+    url: '/mensHero.jpg',
+    title: `Latest Men's Collection`,
+    desc: `Take a look out our latest collection of men's clothing`,
+  },
+  {
+    url: '/womensHero.jpg',
+    title: `Latest Women's Collection`,
+    desc: `Take a look out our latest collection of men's clothing`,
+  },
+  {
+    url: '/accesoriesHero.jpg',
+    title: `Shop for Accessories`,
+    desc: `Check out some of our accessories and boost your style!`,
+  },
+];
