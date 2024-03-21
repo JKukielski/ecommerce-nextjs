64bcae9095a54483bc7c1aad23a9899191409e2e
JKukielski <jankukielski1@gmail.com>
2024-03-21 19:28:06 +0100

    Added hero slider component to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import HeroSlider from '@components/heroSlider/HeroSlider';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home Page
-    </main>
+    <div>
+      <HeroSlider />
+    </div>
   );
 }
