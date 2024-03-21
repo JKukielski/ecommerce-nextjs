04b4aaa705000553ad84de477054460da1a75aef
JKukielski <jankukielski1@gmail.com>
2024-03-21 19:29:20 +0100

    Initial hero section slider setup

---
+++ components/heroSlider/HeroSlider.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import './heroSlider.css';
+import { heroData } from '@utils/data';
+
+const HeroSlider = () => {
+  return <div></div>;
+};
+
+export default HeroSlider;
