a808b646dca3d5bc452d3a89d8c72b7a9d59b4c0
JKukielski <jankukielski1@gmail.com>
2024-03-22 17:45:34 +0100

    Image path correction

--- utils/data.js
+++ utils/data.js
@@ -3,15 +3,18 @@ export const heroData = [
     url: '/mensHero.jpg',
     title: `Latest Men's Collection`,
     desc: `Take a look out our latest collection of men's clothing`,
+    backgroundColor: '#E7E247',
   },
   {
     url: '/womensHero.jpg',
     title: `Latest Women's Collection`,
     desc: `Take a look out our latest collection of men's clothing`,
+    backgroundColor: '#E7E247',
   },
   {
-    url: '/accesoriesHero.jpg',
+    url: '/accessoriesHero.jpg',
     title: `Shop for Accessories`,
     desc: `Check out some of our accessories and boost your style!`,
+    backgroundColor: '#E7E247',
   },
 ];
