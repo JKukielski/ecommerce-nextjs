31661c9561cb3438e07c970c721c73bb6e5586bf
JKukielski <jankukielski1@gmail.com>
2024-03-26 19:15:09 +0100

    Included recommended products component in home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import HeroSlider from '@components/heroSlider/HeroSlider';
+import RecommendedProducts from '@components/recommendedProducts/RecommendedProducts';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <HeroSlider />
+      <RecommendedProducts />
     </div>
   );
 }
