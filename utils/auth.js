68bbefe93434ac8da3fbc879da1a592139600aa4
JKukielski <jankukielski1@gmail.com>
2024-04-23 12:57:02 +0200

    Callback for saving user to DB

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDb } from './database';
+import User from '@models/User';
 
 export const {
   handlers: { GET, POST },
@@ -13,4 +15,27 @@ export const {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === 'google') {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.name,
+              email: profile.email,
+            });
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return true;
+      //   console.log(profile);
+    },
+  },
 });
