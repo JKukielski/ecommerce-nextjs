10c69119e920e1324bbdea762391e1dccdbd0bd3
JKukielski <jankukielski1@gmail.com>
2024-04-24 15:37:10 +0200

    Login page UI

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,26 +1,29 @@
 import { handleGoogleLogin } from '@utils/actions';
 import './login.css';
 import { auth } from '@utils/auth';
+import { FcGoogle } from 'react-icons/fc';
 
 const LoginPage = async () => {
   const session = await auth();
-  console.log(session);
 
   return (
     <div className="login-container">
-      <form action={handleGoogleLogin}>
-        <button>Login with Google</button>
-      </form>
-      <form action="">
-        <label htmlFor="">
-          Username
-          <input type="text" />
-        </label>
-        <label htmlFor="">
-          Password
-          <input type="password" />
-        </label>
-      </form>
+      <div className="login-wrapper">
+        <h2 className="login-form-heading">Welcome back!</h2>
+        <form action={handleGoogleLogin} className="login-google-form">
+          <button>
+            <FcGoogle className="login-google-icon" /> Sign in with Google
+          </button>
+        </form>
+        <form action="" className="login-credentials-form">
+          <input type="text" className="" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+
+          <div className="login-form-btn-container">
+            <button>Sign in</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
