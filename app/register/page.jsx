d4d2b1b6e606c04ae1829c2a139768fd8fbc7222
JKukielski <jankukielski1@gmail.com>
2024-04-24 15:38:46 +0200

    Register page UI

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,22 +1,27 @@
+import { handleGoogleLogin } from '@utils/actions';
 import './register.css';
+import { FcGoogle } from 'react-icons/fc';
 
 const RegisterPage = () => {
   return (
     <div className="register-container">
-      <form action="">
-        <label htmlFor="">
-          Email
-          <input type="email" />
-        </label>
-        <label htmlFor="">
-          Username
-          <input type="text" />
-        </label>
-        <label htmlFor="">
-          Password
-          <input type="password" />
-        </label>
-      </form>
+      <div className="register-wrapper">
+        <h2 className="register-form-heading">Sign up to Shopper!</h2>
+        <form action={handleGoogleLogin} className="register-google-form">
+          <button>
+            <FcGoogle className="register-google-icon" /> Sign in with Google
+          </button>
+        </form>
+        <form action="" className="register-credentials-form">
+          <input type="text" className="" placeholder="Full Name" />
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+
+          <div className="register-form-btn-container">
+            <button>Sign in</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
